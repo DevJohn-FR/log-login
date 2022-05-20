9ca10aa56b45d49e6211d323d3f4829d8d97d531
DevJohn-FR <85360669+DevJohn-FR@users.noreply.github.com>
2022-05-20 21:04:15 +0200

    Update log-login.sh

--- log-login.sh
+++ log-login.sh
@@ -1,30 +1,11 @@
-#!/bin/sh
-
-#
-# title             : ssh_notify
-# description       : Notification lors d'une connexion ssh.
-# author            : TutoRapide
-# date              : 08-01-2021
-# version           : 0.1.0
-# usage             : placer dans /etc/profile.d/ssh-notify.sh
-#===============================================================================
-
-# Config {
-
-        BOTNAME=John-SSH-Login #Nom du webhook
+        BOTNAME=John-SSH-Login
         AVATAR_URL="https://icons.iconarchive.com/icons/blackvariant/button-ui-system-apps/512/Terminal-icon.png"
         WEBHOOK="https://discord.com/api/webhooks/977275800573321306/j0rCIj67VRqhZM2zwErjgPWsX1VztRk5UEG-78foAIsZq6eU-2tD33gBfwrUMzXB5EB4"
-        DATE=$(date +"%d-%m-%Y-%H:%M:%S") #Date + heure
+        DATE=$(date +"%d-%m-%Y-%H:%M:%S")
 
-        TMPFILE=$(mktemp) #Creation d'un fichier temporaire dans /tmp
-
-#~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ }
-
-    IP=`echo $SSH_CLIENT | awk '{ ip = $1 } END { print ip }'`
+    IP=`wget https://ipecho.net/plain -O - -q ; echo'`
 
     curl -s "https://ipapi.co/${IP}/json/" > $TMPFILE
-
-    #On recupére l'opérateur. On supprimer un espace {sed s/' '//g}  est ajoute les double quote {sed s/'"'//g}
 
     ISP=`cat $TMPFILE | jq .org | sed s/' '//g | sed s/'"'//g`
 
@@ -34,7 +15,6 @@
     #On recupére la ville
     VILLE=`cat $TMPFILE | jq -r .city`
 
-    # On récupère le timestamp actuel
     getCurrentTimestamp() { date -u --iso-8601=seconds; };
 
         curl -i --silent \
@@ -46,14 +26,12 @@
             "avatar_url": "'"$AVATAR_URL"'",
             "embeds": [{
                 "color": 12976176,
-                "thumbnail": { "url": "'"$AVATAR_URL"'" },
                 "author": { "name": "Nouvelle connection", "icon_url": "'"$AVATAR_URL"'" },
                 "description": "**Détails**\n \\👤 Utilisateur: '\`$(whoami)\`' \n \\🖥️ Host: '\`$(hostname)\`' \n \\🕐 Connexion: '\`$DATE\`' \n\n **Adresse IP**\n \\📡 IP: '\`${IP}\`' \n \\🌎 Pays: '\`$PAYS\`' \n \\🏙️ Ville: '\`$VILLE\`' \n \\📠 ISP: '\`${ISP}\`'",
                 "timestamp": "'$(getCurrentTimestamp)'"
             }]
         }' $WEBHOOK > /dev/null
 
-# On vient verifier que le fichier temporaire est bien présent puis on le supprime {
 
 checkdir() {
     if [ -e $TMPFILE ]; then
@@ -63,5 +41,3 @@ checkdir() {
     fi
 }
 checkdir
-
-#~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ }
